edebc9e02a481e4c19c3af0ca84d0191a5b52c28
Andrew Nguyen <anguy95@gmail.com>
2022-08-31 23:18:54 -0700

    get 404 data

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,15 @@
-import FourZeroFour from '../components/404';
+import FourZeroFour from "../components/404";
+import { get404 } from "../content-api";
 
 export async function getStaticProps(context) {
+  const page = await get404();
+
   return {
-    props: {  
-      handle: '404',
-      title: 'Oops, not found', 
+    props: {
+      ...page,
       preview: !!context.preview, // will be passed to the page component as props
-    }
-  }
+    },
+  };
 }
 
 export default FourZeroFour;
--- pages/not-found.jsx
+++ pages/not-found.jsx
@@ -1,10 +1,12 @@
 import FourZeroFour from '../components/404';
+import {get404} from '../content-api';
 
 export async function getStaticProps(context) {
+  const page = await get404();
+
   return {
     props: {  
-      handle: '404',
-      title: 'Oops, not found', 
+      ...page,
       preview: !!context.preview, // will be passed to the page component as props
     }
   }
